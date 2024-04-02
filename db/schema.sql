08d48ad0f5a57359d573ed4ce33d214680ff8c9e
Robin <rrlangton@gmail.com>
2024-04-02 17:46:40 -0500

    schema.sql added

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    department VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES deparments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    ON DELETE SET NULL
+);
