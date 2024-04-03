687b4a78d0678a111acb834cc636ce4de7cd3d8c
Robin <rrlangton@gmail.com>
2024-04-03 16:06:01 -0500

    README updated & INSERT INTO fixed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# employee-tracker
+# Employee-Tracker
+
+## Description
+
+This is a command-line application designed to manage a company's employee database, using Node.js, Inquirer, and PostgreSQL.
+
+## Installation
+
+To use this application you must install inquirer and Node.js in the command-line. You can run this application by entering 'npm start' in the command-line. 
+
+## Sources
+
+Instructer help in office hours with async/await queries 
+Tutoring help with writing the index.js
+mini-project help for setting up the schema.sql / seeds.sql
--- index.js
+++ index.js
@@ -46,10 +46,49 @@ inquirer.prompt([
         ])
         .then(async (answers) => {
 
-            const results = await pool.query(`INSERT INTO departments (name) VALUES
-            ('Sales)`, [answers.dept_name]);
+            const results = await pool.query('INSERT INTO departments (name) VALUES ($1)', [answers.dept_name]);
     
             console.log("department added")
         })
     }
-});
+})
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'selection',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a new department',
+            'Add a new role',
+            'Add a new employee',
+            'Update employee role'
+        ]
+    }
+]) 
+.then(async (answers) => {
+    if (answers.selection === 'View all roles') {
+        const results = await pool.query('SELECT * FROM roles');
+
+        console.table(results.rows)
+    }
+
+    if (answers.selection === 'Add a new role') {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "role_name",
+                message: "What is the name of the new role?"
+            }
+        ])
+        .then(async (answers) => {
+
+            const results = await pool.query('INSERT INTO roles (name) VALUES ($1)', [answers.role_name]);
+    
+            console.log("role added")
+        })
+    }
+})
